7892960052e5dba33ee92f115f3df0d41cdbad11
Mmeso1 <anisiubammesoma8@gmail.com>
2025-04-11 17:11:14 +0100

    push updates so far to the main. A lot of fixes to be made

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/nftIssuance" element={<NftIssuance />} />
-          <Route path="/nftIssuance" element={<NftIssuance />} />X
           <Route path="/civic-id-mint" element={<CivicIdMint />} />X
         </Routes>
       </BrowserRouter>
