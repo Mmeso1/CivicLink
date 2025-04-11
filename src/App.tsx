d2bd65b4688c6e19619590fd033c283eb63dcca8
Okpara202 <okparafavour202@gmail.com>
2025-04-11 14:52:56 +0100

    civic id mint page complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import NftIssuance from "./pages/nftIssuance";
+import CivicIdMint from "./pages/civicMint";
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/nftIssuance" element={<NftIssuance />} />
+          <Route path="/nftIssuance" element={<NftIssuance />} />X
+          <Route path="/civic-id-mint" element={<CivicIdMint />} />X
         </Routes>
       </BrowserRouter>
     </>
